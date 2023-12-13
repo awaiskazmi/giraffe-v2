0261046a99a676e63c899dd56f5068252212eaaa
Awais Kazmi <devs.awais@gmail.com>
2023-12-13 09:29:10 +0500

    gate ui improvement

--- vars.js
+++ vars.js
@@ -1,5 +1,5 @@
 export const DAY_COLOR = "#07163D";
 export const NIGHT_COLOR = "#79B7B6";
-export const INTRO_SCROLL_LENGTH = 5000;
+export const INTRO_SCROLL_LENGTH = 4500;
 
 export const ENABLE_RIKSHAW_BOUNCE = true;
